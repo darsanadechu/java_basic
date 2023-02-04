bbf9d925da6ab3fc84a806f186cffba483ba436a
darsa <darsa@LENOVO>
2023-02-04 22:20:19 +0530

    variables and operations

---
+++ sum/src/sum/sum.java
@@ -0,0 +1,21 @@
+package sum;
+
+public class sum {
+	public static void main(String arg[]) {
+		int a=20;
+		int b=30;
+		int c=a+b;
+		float d=(float)c;
+		System.out.println("sum="+c);
+		System.out.println(d);
+		boolean e=true;
+		System.out.println(e);
+		char f='a';
+		System.out.println(f);
+		System.out.println(a++ + ++a);
+		System.out.println(10<<2);
+		System.out.println(40>>2);
+		
+	}
+
+}
