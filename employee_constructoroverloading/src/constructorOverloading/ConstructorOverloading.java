12b944477a121620ab0caacf245fa68b1aec8e84
darsana-temp <darsana-temp@fe80:0:0:0:e4c5:aeff:fea0:f980%4>
2023-02-15 10:45:42 +0530

    constructor overloading

---
+++ employee_constructoroverloading/src/constructorOverloading/ConstructorOverloading.java
@@ -0,0 +1,31 @@
+package constructorOverloading;
+
+class Employee
+{
+	int empid, yearofjoining;
+	String name, company;
+	
+	Employee(int empid,int yearofjoining)
+	{
+		this.empid=empid;
+		this.yearofjoining=yearofjoining;
+	}
+	Employee(String name,String company)
+	{
+		this(7160, 2023);
+		this.name=name;
+		this.company=company;
+	}
+}
+
+public class ConstructorOverloading {
+	public static void main(String[] arg)
+	{
+		Employee E=new Employee("Darsana","Zoho");
+		System.out.println("empid:"+E.empid);
+		System.out.println("comapany:"+E.company);
+		System.out.println("name:"+E.name);
+		System.out.println("year of joining:"+E.yearofjoining);
+	}
+
+}
